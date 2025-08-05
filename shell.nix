3ca73a68242e279b4ab4a1841e4a9415b04a3d8d
Austin Conner <aconner.vu@gmail.com>
2025-08-05 09:57:57 -0400

    old flax

--- shell.nix
+++ shell.nix
@@ -2,27 +2,26 @@ let
   # https://status.nixos.org/ nixos-unstable-small 8/5/25
   # https://hydra.nix-community.org/job/nixpkgs/cuda/python3Packages.torch.x86_64-linux
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/9ebe222ec7ef9de52478f76cba3f0324c1d1119f") { 
-    config.allowUnfree = true;
-    config.cudaSupport = true;
+    # config.allowUnfree = true;
+    # config.cudaSupport = true;
   };
 in
 pkgs.mkShell {
-  buildInputs = with pkgs.python3Packages; [
-    # tensorflow
-    # jax
-    # flax
-    # jax-cuda12-plugin
-    # optax
-    # optimistix
-    # equinox
+  buildInputs = with pkgs.python312Packages; [
+    flax
+    jax-cuda12-plugin
+    optax
+    optimistix
+    equinox
       
-    transformers
     datasets
     evaluate
     scikit-learn
     accelerate
     ipython
-    torch
+    transformers
+      
+    # torch
       
     # venvShellHook
   ];
